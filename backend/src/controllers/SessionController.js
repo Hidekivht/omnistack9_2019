4f6e013fd6da93356f845b2be7768e55e4bef34a
unknown <vht_hideki@hotmail.com>
2019-10-01 19:08:26 -0300

    Criacao do backend

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,21 @@
+// index - listagem de sessões, 
+// show - listagem uma unica sessão, 
+// store - criar uma sessão, 
+// update - alterar uma sessão, 
+// destroy - deletar uma sessão
+
+const User = require('../models/User')
+
+module.exports = {
+  async store(req, res){
+    const { email } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({ email });
+    }
+
+    return res.json(user);
+  }
+};
