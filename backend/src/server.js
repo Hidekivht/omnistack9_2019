451444fae1c308a04fa4a8f75ed9c950cf1adadf
unknown <vht_hideki@hotmail.com>
2019-10-02 19:36:47 -0300

    Frontend Finalizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,7 +13,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-4b1yq.mongodb.net/s
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3335);
